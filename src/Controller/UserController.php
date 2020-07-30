c9394602da368d3b3df9a7a083171d17a87e1ea2
cristian-murcia <djcrissguzman@gmail.com>
2020-07-29 22:31:15 -0500

    Instalacion de dependencias.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,18 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
-class UserController extends AbstractController
-{
-    /**
-     * @Route("/user", name="user")
-     */
-    public function index()
-    {
+class UserController extends AbstractController {
+
+    public function index() {
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
+                    'message' => 'Welcome to your new controller!',
+                    'path' => 'src/Controller/UserController.php',
         ]);
     }
+
 }
--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -3,18 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
-class VideoController extends AbstractController
-{
-    /**
-     * @Route("/video", name="video")
-     */
-    public function index()
-    {
+class VideoController extends AbstractController {
+
+    public function index() {
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/VideoController.php',
+                    'message' => 'Welcome to your new controller!',
+                    'path' => 'src/Controller/VideoController.php',
         ]);
     }
+
 }
